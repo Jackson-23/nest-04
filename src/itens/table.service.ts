162da4e79032439400db72994443c02e9dc9cc63
Jackson <jackson_1998@hotmail.com>
2022-07-06 13:39:17 -0300

    Implement error handling in Itens/table

--- src/itens/table.service.ts
+++ src/itens/table.service.ts
@@ -1,4 +1,4 @@
-import {Get, Injectable, Param} from '@nestjs/common';
+import {Get, Injectable, NotFoundException, Param, UnprocessableEntityException} from '@nestjs/common';
 import {CreateTableDto} from './dto/create-table.dto';
 import { Table } from './entities/table.entity';
 import { PrismaService } from 'src/prisma/prisma.service';
@@ -10,22 +10,49 @@ export class TableService {
     
 
     constructor(private readonly prisma: PrismaService){}
+
+    //Buscar todos os itens
     findAll(): Promise<Table[]>{
         return this.prisma.item.findMany();
     }
 
+    //Busca Item por Id com tratamento de erro
+    async findByIdTry(id: string): Promise <Table>{
+        const record = await this.prisma.item.findUnique({ where: {id}});
+        if(!record){
+            throw new NotFoundException("Register " + id + " not found");
+        }
+        return record;
+    }
+
+    //Buscar Item por ID
     findById(id: string){
-        return this.prisma.item.findUnique({ where: {id}});
+        return this.findByIdTry(id);
     }
 
-    create(createTableDto: CreateTableDto) {
-        return this.prisma.item.create({ data: createTableDto }); 
+    //Criar novo Item
+    create(dto: CreateTableDto) {
+        return this.prisma.item.create({ data: dto }).catch(this.handleError);
     }
 
-    delete(id: string) {
+    //Deletar item por ID
+    async delete(id: string) {
+        await this.findByIdTry(id);
         return this.prisma.item.delete({where: {id}});
     }
-    update(id: string, dto: UpdateTableDto) {
-        return this.prisma.item.update({where: {id}, data: dto})
+
+    //Alterar dados de item por ID
+    async update(id: string, dto: UpdateTableDto) {
+        await this.findByIdTry(id);
+
+        //const data: Partial<Table> = {...dto}
+        return this.prisma.item.update({where: {id}, data: dto}).catch(this.handleError);
+    }
+
+
+
+    handleError(error: Error): undefined{
+        console.log(error.message);
+        throw new UnprocessableEntityException(error.message);
     }
 }
