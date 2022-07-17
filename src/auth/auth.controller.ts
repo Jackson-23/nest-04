2dea34ed32c3de9409ff8491590daae8fdce701d
Jackson-23 <jackson_1998@hotmail.com>
2022-07-17 19:52:33 -0300

    auth: implement Strategy and Guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { LoginResponseDto } from './dto/login.response.dto';
@@ -16,5 +17,12 @@ export default class AuthController {
   })
   login(@Body() loginDto: LoginDto): Promise<LoginResponseDto> {
     return this.authService.login(loginDto);
+  }
+
+  @Get()
+  @UseGuards(AuthGuard())
+  @ApiBearerAuth()
+  profile() {
+    return {mesage:'Atuntenticação feita.'}; 
   }
 }
